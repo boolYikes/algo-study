fc77b69a8a4b3f9f08d96cf2ba79625212ab955e
Dee H Seon <123602432+boolYikes@users.noreply.github.com>
2025-02-21 13:10:51 +0900

    feat: H-Index & The Farthest Node problems

---
+++ programmers/H-Index/boolYikes.py
@@ -0,0 +1,14 @@
+# nerd power index prob
+def solution(citations):
+    target = 0
+    candid = []
+    mx = max(citations)
+    while target <= mx:
+        m = list(map(lambda x: x >= target, citations))
+        # print(target, sum(m)) # map and filter are consummables!
+        if sum(m) >= target:
+            others = [e for e, b in zip(citations, m) if not b]
+            if sum(map(lambda x: x <= target, others)) == len(others):
+                candid.append(target)
+        target += 1
+    return max(candid)
